8414fe61541cfb0acff380a87555ba564c2676de
Beatrice <66280828+ibecoding@users.noreply.github.com>
2024-11-20 22:17:53 -0500

    Initial website files

--- server.js
+++ server.js
@@ -2,24 +2,59 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+// Create server
 const server = http.createServer((req, res) => {
-    if (req.url === '/' || req.url === '/index.html') {
-        const filePath = path.join(__dirname, 'index.html');
-        fs.readFile(filePath, (err, content) => {
-            if (err) {
+    // Resolve file path
+    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+    let extname = path.extname(filePath);
+
+    // Default content type
+    let contentType = 'text/html';
+
+    // Set content type based on file extension
+    switch (extname) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'application/javascript';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+        case '.jpeg':
+            contentType = 'image/jpeg';
+            break;
+        case '.svg':
+            contentType = 'image/svg+xml';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+    }
+
+    // Read and serve the file
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                // File not found
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.end('404 Not Found');
+            } else {
+                // Server error
                 res.writeHead(500, { 'Content-Type': 'text/plain' });
                 res.end('Server Error');
-            } else {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end(content);
             }
-        });
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('404 Not Found');
-    }
+        } else {
+            // Success
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf8');
+        }
+    });
 });
 
+// Start server
 const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
